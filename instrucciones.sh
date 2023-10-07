d682beef1cde056a932f251b73cfb749f1a3d498
JorgeLuis <rodriguezcachique.jorgeluis@gmail.com>
2023-10-07 17:44:24 -0500

    Signed-off-by: JorgeLuis <rodriguezcachique.jorgeluis@gmail.com>

--- instrucciones.sh
+++ instrucciones.sh
@@ -1,12 +1,5 @@
-cd "C:\Users\Jorge Luis\Desktop\CocoBross"
-
+cd "C:\Users\Jorge Luis\Desktop\CocoBross" 
 git add .
-
-current_date=$(date +"%Y-%m-%d %H:%M:%S")
-
-added_files=$(git diff --cached --name-only)
-
-commit_message="$current_date | $added_files"
-
+current_date=$(date +"%Y-%m-%d %H:%M:%S") added_files=$(git diff --cached --name-only) commit_message="$current_date | $added_files"
 git commit -m "$commit_message"
 
