1a7382ac59e87eedf52654cf81de59d8ddbff631
JorgeLuis <rodriguezcachique.jorgeluis@gmail.com>
2023-10-07 18:01:58 -0500

    2023-10-07 18:01:58 | instrucciones.sh

--- instrucciones.sh
+++ instrucciones.sh
@@ -3,3 +3,5 @@ git add .
 current_date=$(date +"%Y-%m-%d %H:%M:%S") added_files=$(git diff --cached --name-only) commit_message="$current_date | $added_files"
 git commit -m "$commit_message"
 
+git push
+
